a771b83707829fa7dfc14106dc4f247f84b7db1c
Mfuon Leonard <mfolee@gmail.com>
2023-04-24 08:42:39 +0300

    added loops

---
+++ loops.py
@@ -0,0 +1,62 @@
+
+# while loop
+
+index = 0
+
+# while index <= 10:
+#     print('*' * index)
+#     index += 1
+#
+# print('done')
+
+# while loop
+# secret_number = 9
+# guess_count = 0
+# guess_limit = 3
+#
+# while guess_count < guess_limit:
+#     guess = int(input('Guess : '))
+#     guess_count += 1
+#
+#     if guess == secret_number:
+#         print('You Win')
+#         break
+# else:
+#     print('You Failed!')
+
+
+# for loop
+
+# for item in 'ItemsList':
+#     print(item)
+#
+# for languages in ['Math', 'English', 'Religion']:
+#     print(languages)
+#
+# for number in range(100, 1000):
+#     print(number)
+
+# prices = [20, 35, 89, 100]
+# total = 0
+# for price in prices:
+#     total += price
+# print(total)
+
+# Nested Loops
+
+# for x in range(4):
+#     for y in range(4):
+#     print(x,y)
+
+numbers = [2, 2, 2, 2, 5]
+for number in numbers:
+    output = ''
+    for x in range(number):
+        output += 'x'
+    print(output)
+#
+# xxxxx
+# xx
+# xxxxx
+# xx
+# xx
