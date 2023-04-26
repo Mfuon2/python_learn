d270003acc1a55ffa5c6fae5cca29bf734e4cbec
Mfuon Leonard <mfolee@gmail.com>
2023-04-26 07:16:20 +0300

    lists and its functions

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-# names = ['Q1', 'Q2', 'Q3', 'Q4']
-# print(names[:])
-# names[2] = 'Mfuon'
-# print(names)
+# # Tuples Similar to list but are immutable. Not able to change or edit
+# has only count and index
 
-# Find the largest number in a list
-# 1D Lists
+numbers = (1, 2, 3)
 
-numbers = [1, 3, 4, 5, 1, 5, 9, 2]
-largest_number = 0
-
-for number in numbers:
-    if number > largest_number:
-        largest_number = number
-
-print(largest_number)
-
-# 2D Lists
-
-matrix = [
-    [1, 2, 3],
-    [1, 4, 5],
-    [5, 6, 8]
-]
+print(numbers[2])
